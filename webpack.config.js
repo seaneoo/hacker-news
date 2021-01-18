12aac00147bce0fcfa67c9a932fc488b1a8ed36a
Sean O'Connor <seanoconnor941@gmail.com>
2021-01-17 19:40:46 -0700

    TypeScript, Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  entry: "./src/index.ts",
+
+  output: {
+    path: dist,
+    filename: "bundle.[contenthash].js",
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: dist,
+    compress: true,
+    port: 3000,
+  },
+
+  optimization: { minimizer: [new UglifyJsPlugin()] },
+
+  module: {
+    rules: [
+      {
+        // TypeScript
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+
+      {
+        // CSS
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
