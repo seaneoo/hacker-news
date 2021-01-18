63b0e7f6f3244cadaaf216bbe284435cb11492ce
Sean O'Connor <seanoconnor941@gmail.com>
2021-01-17 19:40:53 -0700

    Initial code

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+const get = async (url: string): Promise<any> => {
+  return await fetch(url)
+    .then((res) => res.json())
+    .then((json) => json);
+};
+
+export { get };
---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import "./style.css";
+
+import { get } from "./api";
+const topstories = document.getElementById("topstories");
+
+const getDomainFromUrl = (url: string) => {
+  const exp = new RegExp(
+    /^[a-z][a-z0-9+\-.]*:\/\/([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|↵\[[a-z0-9\-._~%!$&'()*+,;=:]+\])/g
+  );
+  return url.match(exp);
+};
+
+const addItem = (post: number) => {
+  get(`https://hacker-news.firebaseio.com/v0/item/${post}.json`).then(
+    (res: {
+      id: number;
+      type: string;
+      url: string;
+      title: string;
+      by: string;
+      score: number;
+      time: number;
+      descendants: number;
+    }) => {
+      const post = document.createElement("div");
+      post.classList.add("post");
+
+      const post_title = document.createElement("span");
+      post_title.classList.add("post_title");
+      post_title.setAttribute("title", res.title);
+
+      const a = document.createElement("a");
+      a.href = res.url;
+      a.innerHTML = `${
+        res.title
+      } <span style="font-size: 65%">(${getDomainFromUrl(res.url)})</span>`;
+      post_title.append(a);
+
+      post.append(post_title);
+
+      const post_info = document.createElement("div");
+      post_info.classList.add("post_info");
+      post.append(post_info);
+
+      const post_points = document.createElement("span");
+      post_points.classList.add("post_points");
+      post_points.setAttribute("title", `${res.score} points`);
+      post_points.innerText = `↑ ${res.score}`;
+      post_info.append(post_points);
+
+      const post_author = document.createElement("span");
+      post_author.classList.add("post_author");
+      post_author.setAttribute("title", `by ${res.by}`);
+      post_author.innerText = `by ${res.by}`;
+      post_info.append(post_author);
+
+      if (res.descendants > 0) {
+        const post_comments = document.createElement("span");
+        post_comments.classList.add("post_comments");
+        post_comments.setAttribute("title", `${res.descendants} comments`);
+        post_comments.innerHTML = `<a href="https://news.ycombinator.com/item?id=${res.id}">${res.descendants} comments</a>`;
+        post_info.append(post_comments);
+      }
+
+      topstories.append(post);
+    }
+  );
+};
+
+(async () => {
+  get("https://hacker-news.firebaseio.com/v0/topstories.json").then(
+    (res: Array<number>) => {
+      for (let i = 0; i < res.length; i++) {
+        // max number of posts to load
+        if (i < 50) addItem(res[i]);
+      }
+    }
+  );
+})();
